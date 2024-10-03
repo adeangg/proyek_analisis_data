6e507d891c303b4222a693932076b60c8de8609f
adeangg <adeputra2253@gmail.com>
2024-10-04 02:45:53 +0700

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+sns.set(style='dark')
+
+st.header('Proyek Analisis Data: Bike Sharing Dataseet')
+
+st.caption('Disusun oleh: Ade Putra Anggoro / m200b4ky0076@bangkit.academy / adeangg')
+
+st.subheader('Komparasi Penyewa Tipe Casual dan Registered')
+
+st.subheader('Analisis Jumlah Penyewa Sepeda Terhadap Musim/Cuaca pada Periode Tertentu')
+
